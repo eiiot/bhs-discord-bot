dc8c318e064530ab0e074d7770c1a09bf5ed6f92
Eliot Hertenstein <eliot.supceo@gmail.com>
2021-08-04 18:17:06 -0500

    Add more embeds and channel id and stuff

--- discord_bot.js
+++ discord_bot.js
@@ -61,11 +61,25 @@ client.on('messageCreate', async message => {
 	}
 
   if (messageContent[0].toLowerCase() === '.studyroom' && message.author.id === client.application?.owner.id) {
-    message.reply(`My commands have moved! Try /studyroom`);
+    const embed = {
+      color: 0xeff624,
+      title: 'My Commands Have Moved!',
+      description: "Try using `/studyroom`. Do `/help` for more information!",
+      timestamp: new Date(),
+    };
+    
+    message.reply({ embeds: [embed] });
 	}
 
   if (messageContent[0].toLowerCase() === '.help' && message.author.id === client.application?.owner.id) {
-    message.reply(`My commands have moved! Try /help`);
+    const embed = {
+      color: 0xeff624,
+      title: 'My Commands Have Moved!',
+      description: "Try using `/help!`",
+      timestamp: new Date(),
+    };
+    
+    message.reply({ embeds: [embed] });
 	}
 });
 
@@ -93,25 +107,36 @@ client.on('interactionCreate', async interaction => {
     console.log(users);
     // create a new voice channel
 
-    if (users === undefined) {
-      const voiceChannel = guild.channels.create(`${interaction.user.username}'s Study Room`, {
-        type: 'GUILD_VOICE',
-        topic: `Created by ${interaction.user.username}`,
-        parent: '762757482274881536',
-      })
-    } else {
-      const voiceChannel = guild.channels.create(`${interaction.user.username}'s Study Room`, {
-        type: 'GUILD_VOICE',
-        topic: `Created by ${interaction.user.username}`,
-        parent: '762757482274881536',
-        userLimit: users.value
-      })
+    // create an empty array
+    const voiceChannel = [];
+
+    async function createChannel() {
+      if (users === undefined) {
+        const channel = await guild.channels.create(`${interaction.user.username}'s Study Room`, {
+          type: 'GUILD_VOICE',
+          topic: `Created by ${interaction.user.username}`,
+          parent: '762757482274881536',
+        })
+        return channel;
+      } else {
+        const channel = await guild.channels.create(`${interaction.user.username}'s Study Room`, {
+          type: 'GUILD_VOICE',
+          topic: `Created by ${interaction.user.username}`,
+          parent: '762757482274881536',
+          userLimit: users.value
+        })
+        return channel;
+      };
     };
+
+    const createdChannel = await createChannel();
+
+    console.log(createdChannel);
 
     const embed = {
       color: 0xeff624,
       title: 'Created a Study Room!',
-      description: `Created the channel ${interaction.user.username}'s Study Room!`,
+      description: `Created the channel <#${createdChannel.id}>`,
       timestamp: new Date(),
     };
 
