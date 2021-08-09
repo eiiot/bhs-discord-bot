530132e8aea1262bd5f678a20518dfd24b5cbe8d
Eliot Hertenstein <39346975+EliotHertenstein@users.noreply.github.com>
2021-08-09 12:53:01 -0700

    Fix Email Stuff

--- email_test.js
+++ email_test.js
@@ -43,16 +43,179 @@ dotenv.config();
 //     }
 //   });
 
-var email = 'eliot.supceo@berkeley.net'
+// var email = 'eliot.supceo@berkeley.net'
 
-function validateBUSDEmail(email) {
-    if (/berkeley.net\s*$/.test(email)) {
-    console.log("it ends in @berkeley.net");
-    return true;
-    } else {
-    console.log("it does not end in @berkeley.net");
-    return false;
+// function validateBUSDEmail(email) {
+//     if (/berkeley.net\s*$/.test(email)) {
+//     console.log("it ends in @berkeley.net");
+//     return true;
+//     } else {
+//     console.log("it does not end in @berkeley.net");
+//     return false;
+//     };
+// };
+
+// validateBUSDEmail(email);
+
+if (interaction.channel.id !== '787039874384396329') {
+    const embed = {
+      color: 0xeff624,
+      title: 'Verification',
+      description: `**${user.tag}** has already been verified!`,
+      timestamp: new Date(),
     };
-};
+    await interaction.reply({ content: { embeds: [embed] },  ephemeral: true });
+    return;
+  };
 
-validateBUSDEmail(email);
+  var email = interaction.options.get('email');
+  console.log(email.value);
+  var email = email.value.toLowerCase();
+
+  var id = interaction.user.id;
+
+  // stop function if email is invalid
+  if (!validateEmail(email)) {
+    const embed = {
+      color: 0xeff624,
+      title: 'Verification',
+      description: `Invalid Email! Please try again.`,
+      timestamp: new Date(),
+    };
+    await interaction.reply({ content: { embeds: [embed] },  ephemeral: true });
+    return;
+  };
+
+  if (!validateBUSDEmail(email)) {
+    const embed = {
+      color: 0xeff624,
+      title: 'Verification',
+      description: `Invalid Email! Please try again.`,
+      timestamp: new Date(),
+    };
+    await interaction.reply({ content: { embeds: [embed] },  ephemeral: true });
+    return;
+  };
+
+  // load emails database
+
+  var emailsJSON = fs.readFileSync('./emails.json', 'utf8');
+  var emailsDatabase = JSON.parse(emailsJSON);
+
+  // see if email appears in array
+  function emailExists(email) {
+    for (var i = 0; i < emailsDatabase.length; i++) {
+      if (emailsDatabase[i].email === email) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  // check if id appears in array
+  function idExists(id) {
+    for (var i = 0; i < emailsDatabase.length; i++) {
+      if (emailsDatabase[i].id === id) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  var emailCheck = emailExists(email);
+  var idCheck = idExists(id);
+
+  if (emailCheck) {
+    const embed = {
+      color: 0xeff624,
+      title: 'Verification',
+      description: `**${email}** has already been verified!`,
+      timestamp: new Date(),
+    };
+    await interaction.reply({ content: { embeds: [embed] },  ephemeral: true });
+    return;
+  };
+
+  if (!idCheck) {
+
+    // send verification email to user
+
+    let code = makeid(5);
+
+    const msg = {
+      to: `${email}`,
+      from: {
+          email: 'discord@bhs.eliothertenstein.com',
+          name: 'Berkeley High Discord'
+      },
+      subject: 'Verify your Discord account!',
+      text: `Hello! Your verification code is: ${code}.\nThis code will expire in 5 minutes!`,
+      html: `Hello! Your verification code is: <strong>${code}</strong>`,
+    };
+
+    sgMail
+      .send(msg)
+      .then(() => {}, error => {
+        console.error(error);
+    
+        if (error.response) {
+          console.error(error.response.body)
+        }
+    });
+
+    // send user DM
+    const embed = {
+      color: 0xeff624,
+      title: 'Verification',
+      description: `I've sent an email to ${email}! Please reply to this DM with the verification code. The code expires in 5 minutes.`,
+      timestamp: new Date(),
+    };
+
+    let initialDM = await user.send({ embeds: [embed] });
+
+    // wait for user to reply with code
+
+    initialDM.channel.awaitMessages(filter, {
+      max: 1,
+      time: 30000,
+      errors: ['time'],
+    }).then(message => async function () {
+      message = message.first();
+      const userCode = message.content;
+      if (userCode === code) {
+
+        // add user to array
+        const newUser = {
+          id: id,
+          email: email,
+          id: id,
+        };
+        emailsDatabase.push(newUser);
+        fs.writeFileSync('./emails.json', JSON.stringify(emailsDatabase));
+
+        // send verification message
+        const embed = {
+          color: 0xeff624,
+          title: 'Verification',
+          description: `**${user.tag}** has been verified!`,
+          timestamp: new Date(),
+        };
+
+        await user.send({ embeds: [embed] });
+    } else {
+      user.send('Code invalid, please try again later!');
+    };
+  });
+  } else {
+    // send verification message
+    const embed = {
+      color: 0xeff624,
+      title: 'Verification',
+      description: `You are already verified!`,
+      timestamp: new Date(),
+    };
+
+    await interaction.reply({content: { embeds: [embed] }, ephemeral: true});
+
+    user.roles.add('762720121205555220');
+  };
