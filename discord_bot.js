8dbaecbef0d7d9ae5ff358f4aad4ae85bd66a0bc
Eliot Hertenstein <39346975+EliotHertenstein@users.noreply.github.com>
2021-08-08 22:41:50 -0700

    Add email verification start

--- discord_bot.js
+++ discord_bot.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
+const sgMail = require('@sendgrid/mail');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const fs = require('fs');
 const client = new Discord.Client({
     intents: [
       "GUILDS",
@@ -27,6 +30,36 @@ const client = new Discord.Client({
 
 dotenv.config();
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// generatre a random string
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+};
+
+// validate an email
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+
+function validateBUSDEmail(email) {
+  if (/berkeley.net\s*$/.test(email)) {
+  return true;
+  } else {
+  return false;
+  };
+};
+
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity(`Helping BHS Students!`, { type: "PLAYING"});
@@ -43,14 +76,14 @@ client.on('messageCreate', async message => {
 	if (messageContent[0].toLowerCase() === '.deploy_slash_command' && message.author.id === client.application?.owner.id) {
 
     const data = {
-			name: 'studyroom',
-			description: 'Create a study room.',
+			name: 'verify',
+			description: 'Verify your discord account!',
       options: [
         {
-        name: 'users',
-        type: 'INTEGER',
-        description: `Limit users. Leave empty for no limit.`,
-        required: false
+        name: 'email',
+        type: 'STRING',
+        description: `Your berkeley.net email!`,
+        required: true
         }                      
       ]
 		};
@@ -85,6 +118,169 @@ client.on('messageCreate', async message => {
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'verify') {
+    const user = interaction.user;
+    const guild = interaction.guild;
+    const channel = interaction.channel;
+    const message = interaction.message;
+
+    if (interaction.channel.id == '787039874384396329') {
+      const embed = {
+        color: 0xeff624,
+        title: 'Verification',
+        description: `**${user.tag}** has already been verified!`,
+        timestamp: new Date(),
+      };
+      await interaction.reply({ embeds: [embed] });
+      return;
+    };
+
+    var email = interaction.options.get('email');
+    console.log(email.value);
+    var email = email.value.toLowerCase();
+
+    var id = interaction.user.id;
+
+    // stop function if email is invalid
+    if (!validateEmail(email)) {
+      return;
+    }
+
+    // stop function if email is already verified
+
+    var emailsJSON = fs.readFileSync('./emails.json', 'utf8');
+    var emailsDatabase = JSON.parse(emailsJSON);
+
+    // see if email appears in array
+    function emailExists(email) {
+      for (var i = 0; i < emailsDatabase.length; i++) {
+        if (emailsDatabase[i].email === email) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    // check if id appears in array
+    function idExists(id) {
+      for (var i = 0; i < emailsDatabase.length; i++) {
+        if (emailsDatabase[i].id === id) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    var emailCheck = emailExists(email);
+    var idCheck = idExists(id);
+
+    if (emailCheck) {
+      const embed = {
+        color: 0xeff624,
+        title: 'Verification',
+        description: `**${email}** has already been verified!`,
+        timestamp: new Date(),
+      };
+      await interaction.reply({ content: { embeds: [embed] },  ephemeral: true });
+      return;
+    };
+
+    if (!idCheck) {
+
+      // send verification email to user
+
+      let code = makeid(5);
+
+      const msg = {
+        to: `${email}`,
+        from: {
+            email: 'discord@bhs.eliothertenstein.com',
+            name: 'Berkeley High Discord'
+        },
+        subject: 'Verify your Discord account!',
+        text: `Hello! Your verification code is: ${code}.\nThis code will expire in 5 minutes!`,
+        html: `Hello! Your verification code is: <strong>${code}</strong>`,
+      };
+
+      sgMail
+        .send(msg)
+        .then(() => {}, error => {
+          console.error(error);
+      
+          if (error.response) {
+            console.error(error.response.body)
+          }
+      });
+
+      // send user DM
+      const embed = {
+        color: 0xeff624,
+        title: 'Verification',
+        description: `I've sent an email to ${email}! Please reply to this DM with the verification code. The code expires in 5 minutes.`,
+        timestamp: new Date(),
+      };
+
+      let initialDM = await user.send({ embeds: [embed] });
+
+      // wait for user to reply with code
+
+      let message = user.awaitMessages({ filter, max: 1, time: 300000, errors: ['time'] })
+      .then(collected => {
+        const message = collected.first();
+        return message
+        })
+      .catch(collected => {
+        user.send('Your time expired. Please try again later!');
+        return false;
+      });
+
+      console.log(message);
+
+      if (message != false) return;
+
+      // check if code is valid
+      const messageCode = message.content;
+
+      if (messageCode === code) {
+        // add user to array
+        const newUser = {
+          id: id,
+          email: email,
+          id: id,
+        };
+        emailsDatabase.push(newUser);
+        fs.writeFileSync('./emails.json', JSON.stringify(emailsDatabase));
+
+        // send verification message
+        const embed = {
+          color: 0xeff624,
+          title: 'Verification',
+          description: `**${user.tag}** has been verified!`,
+          timestamp: new Date(),
+        };
+
+        await user.send({ embeds: [embed] });
+      } else {
+        user.send('Code invalid, please try again later!');
+      };
+    } else {
+      // send verification message
+      const embed = {
+        color: 0xeff624,
+        title: 'Verification',
+        description: `You are already verified!`,
+        timestamp: new Date(),
+      };
+
+      await interaction.reply({content: { embeds: [embed] }, ephemeral: true});
+
+      user.roles.add('762720121205555220');
+    };
+  };
+
+  // not verify
+  if (interaction.channel.id !== '787039874384396329') return;
 
   if (interaction.commandName === 'help') {    
     const helpEmbed = {
