21293f48dacef5cb6756379c5ff1877f18157b90
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-05-03 12:57:41 -0700

    remove extra .js files

--- src/modules/commands/index.js
+++
@@ -1,7 +0,0 @@
-export { default as help } from "./help";
-export { default as user } from "./user";
-export { default as studyroom } from "./studyroom";
-export { default as stats } from "./stats";
-export { default as archive } from "./archive";
-export { default as leaderboard } from "./leaderboard";
-export { default as short } from "./short";
